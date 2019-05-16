ea258dbe606900a5473ecf3859bee045dd0bd05a
Aidan Paetsch <aapaetsch@gmail.com>
2019-05-16 20:56:43 +0000

    some stuff

--- edmontonviz/src/App.js
+++ edmontonviz/src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'shards-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <Button pill theme="light"> Edmonton Bus Locations </Button>
+        <Button pill theme="light"> Edmonton Properties </Button>
+        <Button pill theme="light"> Edmonton Trees </Button>
+        <Button pill theme="light"> Map Testing </Button>
+        </div>
       </header>
     </div>
   );
---
+++ edmontonviz/src/Containers/busviz.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
+
+class Map extends Component{
+
+    state = {
+        viewport:{
+            width: 400,
+            height: 400,
+            latitude
+        }
+    }
+
+
+
+
+}
