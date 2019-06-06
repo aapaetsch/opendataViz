eb846c640446d1a2241a6f983cbd6bb54460851b
Aidan Paetsch <aapaetsch@gmail.com>
2019-06-06 22:27:09 +0000

    testing the link to the bus data

--- edmontonviz/src/Containers/Test.js
+++ edmontonviz/src/Containers/Test.js
@@ -6,16 +6,19 @@ import "./test.css";
 class Test extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            BUS: {},
+        }
     }
-
-
+    getContent(URL){
+        fetch(URL).then(response => this.setState({BUS: response}));
+        console.log(this.state.BUS);
+    }
     render() {
         return(
             <div>
-                <Button className="lol" onClick={()=>{this.props.goHome()}}> GO HOME</Button>
-                <p>
-                    this is a test
-                </p>
+                <Button className="lol" onClick={()=>{this.getContent('https://data.edmonton.ca/api/views/uzpc-8bnm/files/4785ba37-9862-4d26-844f-9aa2dc1c246d?filename=TripUpdates.pb')}}> GO HOME</Button>
+
             </div>
         );
     }
